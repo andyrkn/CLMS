643bbcb01b2c3a179d05a880e7b24242c5d3add0
Verciuc Sergiu <v_sergiu2009@yahoo.com>
2019-01-17 18:47:16 +0200

    Added a couple of unittests.

---
+++ Sources/Notification/CLMS.Notification.Domain.Tests/EmailMessageTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace CLMS.Notification.Domain.Tests
+{
+    public class EmailMessageTests
+    {
+        [Fact]
+        public void Given_Create_When_ParametersArePassed_Then_ShouldCreateEmailMessage()
+        {
+            // act
+            var emailMessage = EmailMessage.Create("test", "Test subject", "hello");
+
+            // assert
+            Assert.NotNull(emailMessage);
+        }
+
+        [Fact]
+        public void Given_Create_When_ParametersAreGiven_Then_ShouldSetParameters()
+        {
+            // arrange
+            var to = "test";
+            var subject = "Test subject";
+            var body = "hello";
+
+            // act
+            var emailMessage = EmailMessage.Create(to, subject, body);
+
+            // assert
+            Assert.Equal(to, emailMessage.To);
+            Assert.Equal(subject, emailMessage.Subject);
+            Assert.Equal(body, emailMessage.Body);
+        }
+    }
+}
