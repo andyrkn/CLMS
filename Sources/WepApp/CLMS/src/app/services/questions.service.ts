bfab60c344bd3198519ec754ae657fdc5dac7eff
Mihaila Andrei <andyfbgm@gmail.com>
2019-01-18 03:15:13 +0200

    header fix

--- Sources/WepApp/CLMS/src/app/services/questions.service.ts
+++ Sources/WepApp/CLMS/src/app/services/questions.service.ts
@@ -4,7 +4,7 @@ import { ServerConfig } from './server.config';
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
 import { AnswerModel } from './Models/answer.model';
-import {QuestionModel} from './Models/question.model';
+import { QuestionModel } from './Models/question.model';
 
 @Injectable()
 export class QuestionsService {
@@ -19,9 +19,9 @@ export class QuestionsService {
         this.authHeaders = new HttpHeaders({ 'Authorization': this.userService.getToken() });
     }
 
-    public CreateQuestion(questionModel : QuestionModel) {
-        const body = {'Name' : questionModel.Name};
-    
+    public CreateQuestion(questionModel: QuestionModel) {
+        const body = { 'Name': questionModel.Name };
+
         this.httpClient.post(ServerConfig.endpoint + this._questions, body, { headers: this.authHeaders })
             .subscribe((res) => {
                 console.log(res);
@@ -40,7 +40,6 @@ export class QuestionsService {
         const answerModel = new AnswerModel(answer, this.userService.getEmail());
         const body = { 'AnswerText': answerModel.AnswerText, 'Email': answerModel.Email };
 
-        console.log(body, questionId);
         this.httpClient.post(ServerConfig.endpoint + this._questions + '/' + questionId + '/answer', body, { headers: this.authHeaders })
             .subscribe((res) => {
                 console.log(res);
@@ -48,6 +47,8 @@ export class QuestionsService {
     }
 
     public ApproveAnswerForAQuestion(questionId: any, answerId: any) {
-        this.httpClient.put(ServerConfig.endpoint+this._questions+ '/' + questionId + '/answer'+ '/'+answerId, { headers: this.authHeaders })
+        this.httpClient
+            .put(ServerConfig.endpoint + this._questions + '/' + questionId + '/answer' + '/' + answerId, { headers: this.authHeaders })
+            .subscribe();
     }
 }
